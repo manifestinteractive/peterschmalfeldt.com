64e0a91ed0f2df531748e0ef5309f31dcb46bc4e
Peter Schmalfeldt <peter@manifestinteractive.com>
2022-06-09 00:49:31 -0400

    Fix typo in Google Analytics ID

--- src/html/data/package.js
+++ src/html/data/package.js
@@ -4,7 +4,7 @@ const env = process.env.NODE_ENV ? process.env.NODE_ENV : 'development'
 const isProd = env === 'production'
 
 module.exports = {
-  analytics: 'C8XZPHRS2H',
+  analytics: 'G-C8XZPHRS2H',
   base: 'https://peterschmalfeldt.com',
   assetPath: '/assets/',
   cacheBreak: isProd ? '' : `?ac${new Date().getTime()}`,
